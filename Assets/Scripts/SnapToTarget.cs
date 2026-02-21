317bc216c9137311767c8b8dbdb904bf1b539fe2
christinaqiu3 <christinaqiu3@gmail.com>
2026-02-21 13:47:42 -0500

    Added heart snap script

---
+++ Assets/Scripts/SnapToTarget.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class SnapToTarget : MonoBehaviour
+{
+    public Transform snapTarget;
+    public float snapSpeed = 10f;
+    public bool snapInstantly = false;
+
+    private bool snapping = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartSnapping();
+        }
+    }
+
+    void StartSnapping()
+    {
+        snapping = true;
+
+        if (snapInstantly)
+        {
+            transform.position = snapTarget.position;
+            transform.rotation = snapTarget.rotation;
+            snapping = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!snapping) return;
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            snapTarget.position,
+            snapSpeed * Time.deltaTime
+        );
+
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            snapTarget.rotation,
+            snapSpeed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, snapTarget.position) < 0.01f)
+        {
+            transform.position = snapTarget.position;
+            transform.rotation = snapTarget.rotation;
+            snapping = false;
+        }
+    }
+}
